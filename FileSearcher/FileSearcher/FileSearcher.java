fbc1ea855406161e2ee1f7fa4be553f87f779b47
mariam-85 <mariam.lofink85@gmail.com>
2022-10-13 23:01:31 +0200

    changes added

---
+++ FileSearcher/FileSearcher/FileSearcher.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSearcher {
+    public static void main(String[] args) {
+        ArrayList<File> fileList = new ArrayList<>();
+        getFiles(new File("C:\\"), fileList);
+        for(File file : fileList) {
+            System.out.println(file.getAbsolutePath());
+        }
+    }
+
+    public static void getFiles(File rootFile, List<File> fileList) {
+        if(rootFile.isDirectory()) {
+            System.out.println("searching...." + rootFile.getAbsolutePath());
+            File[] directoryFiles = rootFile.listFiles();
+            if(directoryFiles != null) {
+                for(File file: directoryFiles) {
+                    if(file.isDirectory()) {
+                        getFiles(file, fileList);
+                        //*******************************************************************************
+                    } else {
+                        if(file.getName().toLowerCase().endsWith(".pdf")) {
+                            fileList.add(file);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lesson3_HW_Task1/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Stack st1 = new Stack();
+        char[] brackets = {'(', ')', '[', ']', '{', '}'};
+        String str = "(([]{}))";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (isOpenBracket(c, brackets)) {
+                st1.push(c);
+            } else {
+                if (st1.isEmpty()) {
+                    System.out.println("Wrong bracket string");
+                    return;
+                }
+                st1.pop();
+            }
+        }
+        if (st1.isEmpty()) {
+            System.out.println("Correct bracket string");
+        } else {
+            System.out.println("Wrong bracket string");
+        }
+    }
+
+    private static boolean isOpenBracket(char c, char[] brackets) {
+        for (int i = 0; i < brackets.length; i += 2) {
+            if (brackets[i] == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // b - открывающаяся скобка
+    // c - закрывающаяся скобка
+    private static boolean isBracketsMatch(char b, char c, char[] brackets) {
+        for (int i = 0; i < brackets.length; i += 2) {
+            if (brackets[i] == b && brackets[i + 1] == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
